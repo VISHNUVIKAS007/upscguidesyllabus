07e9fd7dd78ac36721ded51bde7ba84cc39a22bc
vishnu <vishnu@example.com>
2019-11-18 00:10:41 +0530

    second commit

---
+++ app/src/main/java/com/example/upscguide/AptitudeTest.java
@@ -0,0 +1,13 @@
+package com.example.upscguide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AptitudeTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aptitude_test);
+    }
+}
